45ac97ab012632405cc70302940eaf25b88380e0
Kristafor-Magali <magali.kristafor@ingenieria.uner.edu.ar>
2024-05-15 00:19:29 -0300

    Se suben cambios en el drive LDR

--- firmware/drivers/devices/src/ldr.c
+++ firmware/drivers/devices/src/ldr.c
@@ -1,16 +1,22 @@
 
 /*==================[inclusions]=============================================*/
-#include "servo_sg90.h"
-#include "pwm_mcu.h"
+#include <stdint.h>
+#include <analog_io.h>
+#include "uart_mcu.h"
+#include "analog_io_mcu.h"
+#include "timer.h"
+
 /*==================[macros and definitions]=================================*/
-#define SERVO_FREQ 	50
-#define MIN_ANG		-90
-#define MAX_ANG		90
-#define ANG_RANGE	180.0
-#define PERIOD_MS   20.0
-#define PULSEW_MS   1.0
-#define PERCENT   	100.0
+/*A mayor intensidad de luz mas chica es la resistencia. En oscuridad es mas grande la resistencia*/
+#define LDR_DARK 1000	 /*Resistencia en oscuridad, en KΩ. (~1Mohm)*/
+#define LDR_10LUX 10	 /*Resistencia a 10 lux, en KΩ. (~1.8 Kohm)*/
+#define R_CALIBRACION 47 /*Resistencia calibracion en KΩ*/
+#define LUX_NORMAL 1000	 /*lux normal*/
+
 /*==================[internal data declaration]==============================*/
+analog_input_config_t LDRinput;
+uint16_t luz_aux_dig;
+
 
 /*==================[internal functions declaration]=========================*/
 
@@ -19,57 +25,26 @@
 /*==================[external data definition]===============================*/
 
 /*==================[internal functions definition]==========================*/
-uint8_t Angle2DutyCicle(int8_t angle){
-	static float h_time;
-	static float duty_cicle;
-	static int16_t deg;
-	deg = 2 * angle + MAX_ANG;	// NOTE: adjusted (angle x 2) for the available servos
-	h_time = (float)(deg/ANG_RANGE) + PULSEW_MS;
-	duty_cicle = (float)(h_time/PERIOD_MS) * PERCENT;
-	return (uint8_t) duty_cicle;
-}
+
 /*==================[external functions definition]==========================*/
 
-uint8_t ServoInit(servo_out_t servo, gpio_t gpio){
-	switch(servo){
-		case SERVO_0:
-			PWMInit(PWM_0, gpio, SERVO_FREQ);
-			break;
-		case SERVO_1:
-			PWMInit(PWM_1, gpio, SERVO_FREQ);
-			break;
-		case SERVO_2:
-			PWMInit(PWM_2, gpio, SERVO_FREQ);
-			break;
-		case SERVO_3:
-			PWMInit(PWM_3, gpio, SERVO_FREQ);
-			break;
-	}
-	return 0;
+bool LDR_Init(uint8_t Channel)
+{
+	LDRinput.input = Channel;
+	LDRinput.mode = ADC_SINGLE;
+	LDRinput.func_p = NULL;
+	LDRinput.param_p = NULL;
+	AnalogInputInit(&LDRinput); 
+
+	return true; 
 }
 
-void ServoMove(servo_out_t servo, int8_t ang){
-	static uint8_t dc;
-	if(ang < MIN_ANG){
-		ang = MIN_ANG;
-	} else if(ang > MAX_ANG){
-		ang = MAX_ANG;
-	}
-	dc = Angle2DutyCicle(ang);
-	switch(servo){
-		case SERVO_0:
-			PWMSetDutyCycle(PWM_0, dc);
-			break;
-		case SERVO_1:
-			PWMSetDutyCycle(PWM_1, dc);
-			break;
-		case SERVO_2:
-			PWMSetDutyCycle(PWM_2, dc);
-			break;
-		case SERVO_3:
-			PWMSetDutyCycle(PWM_3, dc);
-			break;
-	}
+uint16_t LDRReadLuxImtensity(void)
+{
+	uint16_t ilum_lux=0;
+	AnalogInputReadSingle(LDRinput.input, &luz_aux_dig);
+	ilum_lux = (luz_aux_dig * LDR_DARK * 10) / (LDR_10LUX * R_CALIBRACION * (1024 - luz_aux_dig)); 
+	return ilum_lux; 
+	
 }
-
 /*==================[end of file]============================================*/
